7da6013c3f6ccfba3d5339c42ac9ec2cac1d4856
batovpasha <batovpasha@gmail.com>
2019-05-19 23:55:10 +0300

    Added some performance cases for the bubble sort algorithm

--- Sorting-algorithms/bubbleSort.js
+++ Sorting-algorithms/bubbleSort.js
@@ -1,5 +1,14 @@
 'use strict';
 
+/*
+    worst-case performance
+  = average-case performance: O(n^2) comparisons, O(n^2) swaps
+    
+    best-case performance:  O(n) comparisons, O(1) swaps
+
+    worst-case space complexity: O(1)
+*/
+
 const bubbleSort = array => {
   const n = array.length;
   for (let i = 0; i < n - 1; i++) {
